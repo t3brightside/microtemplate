f352f4b1f8345bf64da4e7523ddb5bd19ef1fedf
Tanel Põld <tanel@brightside.ee>
2018-03-28 19:09:45 +0800

    Scroll in effect

--- Resources/Public/JavaScript/Page/scroll-effects.js
+++ Resources/Public/JavaScript/Page/scroll-effects.js
@@ -1,18 +1,21 @@
-var $window = $(window);
-var $effectMe = $('.frame');
+/***
+	Set .in-viewport class to every .frame to create fade in effect with CSS
+***/
 
-$window.scroll(function () {
-    var bottom_of_window = $window.scrollTop() + $window.height();
-    $effectMe.each(function (i) {
-        var $elm = $effectMe.eq(i);
-        var top_of_object = $elm.offset().top + 150;
-        if (bottom_of_window > top_of_object) {
-            if (!$elm.hasClass('in-viewport')) {
-                $elm.addClass('in-viewport');
-                $elm.velocity('transition.slideUpIn', { stagger: 10 }).delay(1000);
-            }
-        } else {
-            $elm.removeClass('in-viewport');
-        }
-    }); 
-});
+	var $window = $(window);
+	var $effectMe = $('.frame');
+	var $windowHeight = $window.height();
+	$window.scroll(function () {
+	  var bottom_of_window = $window.scrollTop() + $windowHeight;
+	  $effectMe.each(function (i) {
+	    var $elm = $effectMe.eq(i);
+	    var top_of_object = $elm.offset().top + 150;
+	    if (bottom_of_window > top_of_object) {
+	      if (!$elm.hasClass('in-viewport')) {
+	        $elm.addClass('in-viewport');
+	      }
+	    } else {
+	      $elm.removeClass('in-viewport');
+	    }
+	  }); 
+	});
