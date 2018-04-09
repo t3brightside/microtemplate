af037930cdaf41a302c67bcf7b6d0202ab198e82
Tanel Põld <tanel@brightside.ee>
2018-04-10 02:04:49 +0800

    Update

--- Extensions/pagelist/Configuration/PageTS/setup.ts
+++ Extensions/pagelist/Configuration/PageTS/setup.ts
@@ -1,3 +1,7 @@
+# Removes third template from Template drop down in content element settings
+ TCEFORM.tt_content.tx_pagelist_template.removeItems = 2
+
+ # Adds new template to content element Template drop down
  TCEFORM.tt_content.tx_pagelist_template.addItems {
 		3 = Articles, no img (microtemplate)
  }
