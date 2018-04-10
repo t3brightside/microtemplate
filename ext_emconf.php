eac89573814f383c77c14e0ff01af89da403757f
Tanel Põld <tanel@brightside.ee>
2018-04-10 11:36:25 +0800

    Cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,28 +1,24 @@
 <?php
-
-/**
- * Extension Manager/Repository config file for ext "microtemplate".
- */
-$EM_CONF[$_EXTKEY] = [
-  'title' => 'Microtemplate',
-  'description' => 'One page template with sections and lightbox like sub pages. Demo at: https://microtemplate.t3brightside.com',
-  'category' => 'templates',
-  'constraints' => [
-    'depends' => [
-      'typo3' => '8.7.0-8.7.99',
-      'fluid_styled_content' => '8.7.0-8.7.99',
-      'rte_ckeditor' => '8.7.0-8.7.99',
-      'gridelements' => '8.0.0-8.99.99',
+  $EM_CONF[$_EXTKEY] = [
+    'title' => 'Microtemplate',
+    'description' => 'One page template with sections and lightbox like sub pages. Demo at: https://microtemplate.t3brightside.com',
+    'category' => 'templates',
+    'constraints' => [
+      'depends' => [
+        'typo3' => '8.7.0-8.7.99',
+        'fluid_styled_content' => '8.7.0-8.7.99',
+        'rte_ckeditor' => '8.7.0-8.7.99',
+        'gridelements' => '8.0.0-8.99.99',
+      ],
+      'conflicts' => [
+      ],
     ],
-    'conflicts' => [
-    ],
-  ],
-  'state' => 'beta',
-  'uploadfolder' => 0,
-  'createDirs' => '',
-  'clearCacheOnLoad' => 1,
-  'author' => 'Tanel Põld',
-  'author_email' => 'tanel@brightside.ee',
-  'author_company' => 'Brightside OÜ',
-  'version' => '0.1.2',
-];
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Tanel Põld',
+    'author_email' => 'tanel@brightside.ee',
+    'author_company' => 'Brightside OÜ',
+    'version' => '0.1.2',
+  ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,4 @@
 <?php
 	defined('TYPO3_MODE') || die();
-
-	/* Add RTE configuration */
-
+	
 	$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['microtemplate'] = 'EXT:microtemplate/Configuration/RTE/Default.yaml';
