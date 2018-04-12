b5507dc759c2600ee2ca13a316a0666d28eadce5
Tanel Põld <tanel@brightside.ee>
2018-04-13 00:56:45 +0800

    Enable few more fields

--- Configuration/PageTS/article_page.ts
+++ Configuration/PageTS/article_page.ts
@@ -1,7 +1,7 @@
 # article pages configuration
 	TCEFORM {
 		pages {
-	    title.label = News article title
+	    title.label = Article title
 	    subtitle.label = Sub note (event date, location etc...)
 	    abstract.label = Teaser (showin in news list)
 	    lastUpdated.label = Article Date
@@ -12,16 +12,16 @@
 	    tx_advancedtitle_prefix.disabled = 1
 	    tx_advancedtitle_sufix.disabled = 1
 	    content_from_pid.disabled = 1
-	    target.disabled = 1
+	    target.disabled = 0
 	    cache_timeout.disabled = 1
 	    cache_tags.disabled = 1
 	    no_cache.disabled = 1
 	    is_siteroot.disabled = 1
-	    no_search.disabled = 1
+	    no_search.disabled = 0
 	    php_tree_stop.disabled = 1
 	    module.disabled = 1
 	    categories.disabled = 0
-	    media.disabled = 1
+	    media.disabled = 0
 	    tx_metaplus_favicon.disabled = 1
 		}
 	}
