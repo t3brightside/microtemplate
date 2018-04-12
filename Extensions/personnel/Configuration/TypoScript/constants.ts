5e8bbdfce98d3bbdea71fa1aff58bb8e684a2289
Tanel Põld <tanel@brightside.ee>
2018-04-12 11:46:26 +0800

    Cleaning custom template implementation

--- Extensions/personnel/Configuration/TypoScript/constants.ts
+++ Extensions/personnel/Configuration/TypoScript/constants.ts
@@ -1,3 +1,3 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:personnel/Configuration/TypoScript/constants.ts">
 personnel.stylesPath = EXT:microtemplate/Extensions/personnel/Resources/Public/Styles/personnel.css
-personnel.templateRootPaths = EXT:microtemplate/Extensions/personnel/Resources/Private/Templates/
+personnel.partialRootPaths = EXT:microtemplate/Extensions/personnel/Resources/Private/Partials/
