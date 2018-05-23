d85d7c628ed94bd33d4ab542e1759bd481127e54
Tanel Põld <tanel@brightside.ee>
2018-05-23 12:32:28 +0300

    /cookies to /privacy

--- Extensions/cookie_consent/Configuration/TypoScript/constants.ts
+++ Extensions/cookie_consent/Configuration/TypoScript/constants.ts
@@ -2,11 +2,11 @@
 # cat=Microtemplate/microtemplatexcc; type=string; label= Cookie message
 	microtemplate.ccmessage = Cookies simplify the provision of our services. By using our services you accept our use of cookies.
 # cat=Microtemplate/microtemplatexcc; type=string; label= Read more link text
-	microtemplate.ccmore = More&nbsp;information
+	microtemplate.ccmore = Information & Settings
 # cat=Microtemplate/microtemplatexcc; type=string; label= Dismiss button text
 	microtemplate.ccdismiss = OK
 # cat=Microtemplate/microtemplatexcc; type=string; label= URL to read more page
-	microtemplate.cchref = /cookies/
+	microtemplate.cchref = /privacy/
 
 #[globalVar = GP:L = 1]
 #	microtemplate.ccmessage = Cookies erleichtern die Bereitstellung unserer Dienste. Mit der Nutzung unserer Dienste erklären Sie sich damit einverstanden, dass wir Cookies verwenden.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
