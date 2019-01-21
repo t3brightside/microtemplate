613ca77451be4100c579f41cfe6b7017f521702e
Tanel Põld <tanel@brightside.ee>
2019-01-21 09:15:47 +0800

    Clean up

--- Resources/Public/JavaScript/Page/all.js
+++ Resources/Public/JavaScript/Page/all.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 });
 
 // smooth scroll to anchor links
+var currentHref = window.location.href;
 $('a[href*="#c"]:not([href="#c"])').on('click', function(event) {
   event.preventDefault();
   $('html,body').animate({
--- Resources/Public/JavaScript/Page/home.js
+++ Resources/Public/JavaScript/Page/home.js
@@ -1,51 +1,47 @@
 	$(window).scroll(function() {
+		/*
+		Set #header to .scrolled
+		*/
+		var scroll = $(window).scrollTop();
+		if (scroll >= 45) {
+			$("#header").addClass("scrolled");
+			$(".header-bg").addClass("show");
+		} else {
+			$("#header").removeClass("scrolled");
+			if (!$('#menu').hasClass("mobile")) {
+				$(".header-bg").removeClass("show");
+			}
+		}
 
-	  /*****
-	  	Set #header to .scrolled
-	  *****/
-	  var scroll = $(window).scrollTop();
-	  if (scroll >= 45) {
-	    $("#header").addClass("scrolled");
-	    $(".header-bg").addClass("show");
-	  } else {
-	    $("#header").removeClass("scrolled");
-	    if (!$('#menu').hasClass("mobile")) {
-	      $(".header-bg").removeClass("show");
-	    }
-	  }
-
-	  /*****
-	  	play background videos only if in viewport
-	  *****/
-
-	  $('.bg-video').each(function() {
-	    var top_of_element = $(this).offset().top;
-	    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-	    var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
-	    var top_of_screen = $(window).scrollTop();
-	    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-	      $(this)[0].play();
-	    } else {
-	      $(this)[0].pause();
-	    }
-	  });
-
+		/*
+		play background videos only if in viewport
+		*/
+		$('.bg-video').each(function() {
+		    var top_of_element = $(this).offset().top;
+		    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+		    var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
+		    var top_of_screen = $(window).scrollTop();
+		    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
+		    	$(this)[0].play();
+		    } else {
+		    	$(this)[0].pause();
+		    }
+		});
 	});
 
-	/*****
-	 Keep scroll position after post-back / postback / refresh.
-	*****/
-
+	/*
+	Keep scroll position after post-back / postback / refresh.
+	*/
 	(function($) {
-	  window.onbeforeunload = function(e) {
-	    window.name += ' [' + $(window).scrollTop().toString() + '[' + $(window).scrollLeft().toString();
-	  };
-	  $.maintainscroll = function() {
-	    if (window.name.indexOf('[') > 0) {
-	      var parts = window.name.split('[');
-	      window.name = $.trim(parts[0]);
-	      window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
-	    }
-	  };
-	  $.maintainscroll();
+		window.onbeforeunload = function(e) {
+			window.name += ' [' + $(window).scrollTop().toString() + '[' + $(window).scrollLeft().toString();
+		};
+		$.maintainscroll = function() {
+			if (window.name.indexOf('[') > 0) {
+				var parts = window.name.split('[');
+				window.name = $.trim(parts[0]);
+				window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
+			}
+		};
+		$.maintainscroll();
 	})(jQuery);
