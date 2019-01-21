af23028fed88dca152ec709c273af899d1e05cc7
Tanel Põld <tanel@brightside.ee>
2019-01-21 09:29:35 +0800

    Clean up

--- Resources/Public/JavaScript/Page/scroll-effects.js
+++ Resources/Public/JavaScript/Page/scroll-effects.js
@@ -1,29 +1,27 @@
-/***
-	Set .in-viewport class to every .frame to create fade in effect with CSS
-***/
-
+/*
+Set .in-viewport class to every .frame to create fade in effect with CSS
+*/
 var $addTo = $('.frame');
-
 $.fn.addInViewport = function() {
-  var $effectMe = $(this);
-  var bottom_of_window = $(window).scrollTop() + $(window).height();
-  $(this).each(function(i) {
-    var $elm = $effectMe.eq(i);
-    var top_of_object = $elm.offset().top + 50;
-    if (bottom_of_window > top_of_object) {
-      if (!$elm.hasClass('in-viewport')) {
-        $elm.addClass('in-viewport');
-      }
-    } else {
-      $elm.removeClass('in-viewport');
-    }
-  });
+    var $effectMe = $(this);
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    $(this).each(function(i) {
+        var $elm = $effectMe.eq(i);
+        var top_of_object = $elm.offset().top + 50;
+        if (bottom_of_window > top_of_object) {
+            if (!$elm.hasClass('in-viewport')) {
+                $elm.addClass('in-viewport');
+            }
+        } else {
+            $elm.removeClass('in-viewport');
+        }
+    });
 }
 
 $(window).scroll(function() {
-  $addTo.addInViewport();
+    $addTo.addInViewport();
 });
 
 $(window).on('pageshow', function() {
-  $addTo.addInViewport();
+    $addTo.addInViewport();
 });
