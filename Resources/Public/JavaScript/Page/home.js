2a9ca6a94b2d831e588ab10556c11ec6bd25165d
Tanel Põld <tanel@brightside.ee>
2019-09-23 10:55:15 +0300

    Read section height fix

--- Resources/Public/JavaScript/Page/home.js
+++ Resources/Public/JavaScript/Page/home.js
@@ -33,7 +33,7 @@ $(window).scroll(function() {
     });
     $('.ge-section').each(function() {
         var top_of_element = $(this).offset().top;
-        var bottom_of_element = $(this).offset().top + $(this).height();
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
         var bottom_of_screen = $(window).scrollTop() + window.innerHeight;
         var top_of_screen = $(window).scrollTop();
         if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
