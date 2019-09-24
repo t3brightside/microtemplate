72e5069817bcc51f4d97b8e62ea342d4e09f60a0
Tanel Põld <tanel@brightside.ee>
2019-09-24 14:26:16 +0300

    Improve form sbumission and validation

--- Resources/Public/JavaScript/Page/forms.js
+++ Resources/Public/JavaScript/Page/forms.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
 /* html5 based validation CSS classes & disable send button after submit*/
-    $('select, input, textarea').on('blur', function() {
+    $('.frame-type-form_formframework select, .frame-type-form_formframework input, .frame-type-form_formframework textarea').on('blur', function() {
         if (!$(this).val()) {} else {
-            if ($(this).is(":invalid")) {
-                $(this).addClass("error")
+            if ($(this).is(':invalid')) {
+                $(this).addClass('error')
             }
-            if ($(this).is(":valid") && $(this).hasClass('error')) {
-                $(this).removeClass("error");
-                $(this).addClass("valid");
+            if ($(this).is(':valid') && $(this).hasClass('error')) {
+                $(this).removeClass('error');
+                $(this).addClass('valid');
             }
-            if ($(this).is(":valid")) {
-                $(this).addClass("valid");
+            if ($(this).is(':valid')) {
+                $(this).addClass('valid');
             }
         }
     });
-    $("form .btn").click(function() {
+
+    $('.frame-type-form_formframework form button[type="submit"]').click(function() {
         if (!$(this).parents('form')[0].checkValidity()) {
             $(this).closest('form').find('input,textarea,select').each(function() {
-                if ($(this).is(":invalid")) {
-                    $(this).addClass("error")
+                if ($(this).is(':invalid')) {
+                    $(this).addClass('error')
                 }
             });
-            $(this).closest('form').reportValidity();
-            return false;
         } else {
-            $(this).html("Sending - please wait...");
-            $(this).click();
-            $(this).attr("disabled", true);
+            $(this).hide();
+            $(this).after( "<b>Sending - please wait...</b>" );
         }
     });
 });
