40431dccb08722ad19c01d53f641c9335ec60048
Tanel Põld <tanel@brightside.ee>
2021-11-11 01:30:33 +0200

    [TASK] Clean up and getting ready for next release

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,20 +2,18 @@
   $EM_CONF[$_EXTKEY] = [
     'title' => 'Microtemplate',
     'description' => 'One page template with sections and lightbox like sub pages. Demo at: https://microtemplate.t3brightside.com',
-    'category' => 'templates',
-    'constraints' => [
-      'depends' => [
-        'typo3' => '9.5.0-11.5.99',
-        'fluid_styled_content' => '9.5.0-11.5.99',
-        'gridelements' => '9.0.0-11.5.99',
-      ],
-    ],
+    'category' => 'fe',
+    'version' => '3.0.0',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'author' => 'Tanel Põld',
     'author_email' => 'tanel@brightside.ee',
     'author_company' => 'Brightside OÜ',
-    'version' => '2.1.2',
+    'constraints' => [
+      'depends' => [
+        'typo3' => '11.5.0-11.5.99',
+        'fluid_styled_content' => '11.5.0-11.5.99',
+        'gridelements' => '11.0.0-11.99.99',
+      ],
+    ],
   ];
