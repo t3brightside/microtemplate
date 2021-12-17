df4a030c78e55ae3693bf0d6c7d47634bb7be0bb
Tanel Põld <tanel@brightside.ee>
2021-12-17 11:35:25 +0200

    [TASK] Add content_defender dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,8 @@
       'depends' => [
         'typo3' => '11.5.0-11.5.99',
         'fluid_styled_content' => '11.5.0-11.5.99',
-        'container' => '1.4.0-1.4.99',
+        'container' => '1.4.0-1.99.99',
+        'content-defender' => '3.2.0-3.99.99',
       ],
     ],
   ];
