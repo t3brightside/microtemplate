024257c2e62140d9c5e4ba7acea91d47d5c75e92
Tanel Põld <tanel@brightside.ee>
2021-12-18 16:55:00 +0200

    [BUGFIX] Only add in viewport on accordion click not remove

--- Resources/Public/JavaScript/Page/fadeInContent.js
+++ Resources/Public/JavaScript/Page/fadeInContent.js
@@ -2,7 +2,7 @@
 Set .in-viewport class to every .frame to create fade in effect with CSS
 */
 var $addTo = $('.frame');
-$.fn.addInViewport = function() {
+$.fn.addRemoveInViewport = function() {
   var $effectMe = $(this);
   var bottom_of_window = $(window).scrollTop() + $(window).height();
   $(this).each(function(i) {
@@ -18,12 +18,26 @@ $.fn.addInViewport = function() {
   });
 }
 
+$.fn.addInViewport = function() {
+  var $effectMe = $(this);
+  var bottom_of_window = $(window).scrollTop() + $(window).height();
+  $(this).each(function(i) {
+    var $elm = $effectMe.eq(i);
+    var top_of_object = $elm.offset().top + 50;
+    if (bottom_of_window > top_of_object) {
+      if (!$elm.hasClass('in-viewport')) {
+        $elm.addClass('in-viewport');
+      }
+    }
+  });
+}
+
 $(window).scroll(function() {
-  $addTo.addInViewport();
+  $addTo.addRemoveInViewport();
 });
 
 $(window).on('pageshow', function() {
-  $addTo.addInViewport();
+  $addTo.addRemoveInViewport();
 });
 
 $('.containerAccordion-title').click(function() {
