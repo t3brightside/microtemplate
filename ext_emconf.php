d7dce93566d79042847b94b56dc24048bde1e68a
Tanel Põld <tanel@brightside.ee>
2022-09-08 10:20:52 +0300

    [TASK] Release 3.3.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
     'title' => 'Microtemplate',
     'description' => 'One page template with sections and lightbox like sub pages. Demo at: https://microtemplate.t3brightside.com',
     'category' => 'fe',
-    'version' => '3.2.1',
+    'version' => '3.3.0',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => 'Tanel Põld',
