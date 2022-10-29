fd8e62782d34b2c45a978803b0356d4c77a3bdc0
Tanel Põld <tanel@brightside.ee>
2022-10-29 07:43:31 +0300

    [TASK] Getting rid of jQuery

--- Resources/Public/JavaScript/Page/home.js
+++ Resources/Public/JavaScript/Page/home.js
@@ -1,7 +1,5 @@
-
-var sp = window.scrollY;
-window.addEventListener('scroll', function() {
-  sp = window.scrollY;
+function addScrolledToHeader(el){
+  let sp = window.scrollY;
   if (sp >= 45) {
     header.classList.add('scrolled');
     header.classList.add('force-bg');
@@ -11,19 +9,21 @@ window.addEventListener('scroll', function() {
       header.classList.remove('force-bg');
     }
   }
+}
+
+addScrolledToHeader(header);
+window.addEventListener('scroll', function() {
+  addScrolledToHeader(header);
 });
 
-// keep scroll position after post-back / postback / refresh.
-(function($) {
-  window.onbeforeunload = function(e) {
-    window.name += ' [' + $(window).scrollTop().toString() + '[' + $(window).scrollLeft().toString();
-  };
-  $.maintainscroll = function() {
-    if (window.name.indexOf('[') > 0) {
-      var parts = window.name.split('[');
-      window.name = $.trim(parts[0]);
-      window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
-    }
-  };
-  $.maintainscroll();
-})(jQuery);
+/* remember scroll position */
+window.onbeforeunload = function(e) {
+  window.name += ' [' + window.scrollY.toString() + '[' + window.scrollX.toString();
+  document.querySelector('html').style.cssText = '';
+};
+
+if (window.name.indexOf('[') > 0) {
+  let parts = window.name.split('[');
+  window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
+  document.querySelector('html').style.cssText = 'scroll-behavior: smooth';
+}
--- Resources/Public/JavaScript/Page/menus.js
+++ Resources/Public/JavaScript/Page/menus.js
@@ -1,4 +1,3 @@
-
 function removeMenuClasses() {
   hmbrgr.classList.remove('open');
   menu.classList.remove('open');
@@ -22,40 +21,30 @@ logo.addEventListener('click', function() {
   removeMenuClasses();
 }, false);
 
-// set active menu item by scrolling to a section
-$(window).ready(function(jQuery) {
-  $(window).scrollTop($(window).scrollTop() + 1);
-  var topMenu = jQuery("#menu"),
-    offset = 70,
-    menuItems = topMenu.find('a[href*="#"]'),
-    scrollItems = menuItems.map(function() {
-      var href = jQuery(this).attr("href"),
-        id = href.substring(href.indexOf('#')),
-        item = jQuery(id);
-      if (item.length) {
-        return item;
-      }
-    });
-  $(window).scroll(function() {
-    var fromTop = jQuery(this).scrollTop() + offset;
-    var cur = scrollItems.map(function() {
-      if (jQuery(this).offset().top < fromTop) return this;
-    });
-    cur = cur[cur.length - 1];
-    var id = cur && cur.length ? cur[0].id : "";
-    menuItems.parent().removeClass("active");
-    if (id) {
-      menuItems.parent().end().filter("[href$='#" + id + "']").parent().addClass("active");
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    const id = entry.target.id;
+    const el = document.body.querySelector('#menu a[href*="#' + id + '"]');
+    const active = document.querySelectorAll('#menu .active');
+    const count = active.length;
+    if (entry.isIntersecting) {
+      if (el) {
+      el.parentNode.classList.add('active');
+    }
+      active.forEach(i => {
+        if (i != el) {
+          i.classList.remove('active');
+        }
+      });
+
     }
   });
-
-  function checkVisible(elm, eval) {
-    eval = eval || "object visible";
-    var viewportHeight = $(window).height(), // Viewport Height
-      scrolltop = $(window).scrollTop(), // Scroll Top
-      y = $(elm).offset().top,
-      elementHeight = $(elm).height();
-    if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
-    if (eval == "above") return ((y < (viewportHeight + scrolltop)));
-  }
+};
+const sections2 = document.querySelectorAll('.c-section');
+const options = {
+  threshold: 0.48
+};
+const observer = new IntersectionObserver(callback, options);
+sections2.forEach(section2 => {
+  observer.observe(section2);
 });
