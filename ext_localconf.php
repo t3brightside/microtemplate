d5d93130f2f27314ed17eb47497a5eea334b39a8
Tanel Põld <tanel@brightside.ee>
2023-01-24 00:54:43 +0200

    [TASK] Restore microtemplate RTE conf for v11

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,18 @@
 <?php
 defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
+
 if (empty($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['microtemplate'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['microtemplate'] = 'EXT:microtemplate/Configuration/RTE/Microtemplate.yaml';
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+    if ($versionInformation->getMajorVersion() >= 12) {
+        $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['microtemplate'] = 'EXT:microtemplate/Configuration/RTE/Microtemplate.yaml';
+    } else {
+        $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['microtemplate'] = 'EXT:microtemplate/Configuration/RTE/Microtemplate_11LTS.yaml';
+    }
 }
+
 # Check if ext:personnel is loaded and add TS constant
 if (TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('personnel')) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
