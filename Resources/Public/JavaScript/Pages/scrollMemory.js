a8c45d1ee9dee5621d9171626478322661c97c0c
Tanel Põld <tanel@brightside.ee>
2023-12-28 11:52:57 +0200

    [TASK] Use session storage for remembering scroll position

--- Resources/Public/JavaScript/Pages/scrollMemory.js
+++ Resources/Public/JavaScript/Pages/scrollMemory.js
@@ -1,12 +1,22 @@
+// Remember window scroll position
+// Generate a unique key for the current page in the current tab
+function getPageKey() {
+  return 'scrollPosition_' + window.location.href;
+}
 
-/* remember window scroll position */
-window.onbeforeunload = function (e) {
-  window.name += ' [' + window.scrollY.toString() + '[' + window.scrollX.toString();
+// Remember window scroll position
+window.addEventListener('beforeunload', function () {
+  const pageKey = getPageKey();
+  sessionStorage.setItem(pageKey, JSON.stringify({ x: window.scrollX, y: window.scrollY }));
   document.querySelector('html').style.cssText = '';
-};
+});
 
-if (window.name.indexOf('[') > 0) {
-  let parts = window.name.split('[');
-  window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
+// Check if there is a stored scroll position for the current page in the current tab
+const pageKey = getPageKey();
+const storedScrollPosition = sessionStorage.getItem(pageKey);
+
+if (storedScrollPosition) {
+  const { x, y } = JSON.parse(storedScrollPosition);
+  window.scrollTo(x, y);
   document.querySelector('html').style.cssText = 'scroll-behavior: smooth';
 }
